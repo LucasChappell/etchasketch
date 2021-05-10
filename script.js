9cb809c50b2e5def280b7e42a42b3354609e5e69
Lucas Chappell <chappell1819816@lcsd2kids.org>
2021-05-10 09:21:22 -0600

    rainbow doesnt work

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
 
 const container = document.querySelector('#container')
-function createBox(){
-    for (i = 0; i < 256; i++){
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.id = 'box'
-        container.appendChild(box);
+let root = document.documentElement;
+let penColor = 'black'
+let color = "#3345FF"
+function events(){
+    document.getElementById('blue').addEventListener("click", function(){
+        penColor = 'blue'
     }
+    )
+    document.getElementById('black').addEventListener("click", function(){
+        penColor = 'black'
+    }
+    )
+    document.getElementById('red').addEventListener("click", function(){
+        penColor = 'red'
+    }
+    )
+    document.getElementById('green').addEventListener("click", function(){
+        penColor = 'green'
+    }
+    )
+    document.getElementById('white').addEventListener("click", function(){
+        penColor = 'white'
+    }
+    )
+    document.getElementById('rainbow').addEventListener("click", function(){
+        penColor = color
+    }
+    )
 }
-createBox()
 
-function addClass(){
-    box.classList.add('boxBlack')
+function createBox(dimension){
+    root.style.setProperty('--row-height',(100/dimension) + "%");
+    let autos = "";
+    for (k = 0; k < dimension; k++){
+        autos += " auto";
+    }
+    root.style.setProperty('--cols',autos);
+    for (j = 0; j < dimension; j++){
+        const row = document.createElement("div");
+        for (i = 0; i < dimension; i++){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            
+            box.addEventListener("mouseover", function() {
+                box.style.backgroundColor = penColor
+            })
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    } 
+}
+
+function clear(){
+   document.getElementById('clear').addEventListener("click", function(){
+        container.innerHTML= '';
+    })
 }
 
 
-const mouseTarget = document.getElementById('box')
-mouseTarget.addEventListener('mouseover', function() {
-    plsWork()
-})
 
-
-
-function plsWork(){
-    console.log('hello?')
-}
-
-
-
-//const div1 = document.createElement('div')
-//div1.classList.add('box1')
-//container.appendChild(div1)
+events()
+createBox(160)
+clear()
